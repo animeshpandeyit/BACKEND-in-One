3e88c1d67e84a3edf7be23769fbcb6ecc48ee4b1
ANIMESH PANDEY <animeshpandeyit@gmail.com>
2024-04-15 01:02:18 +0530

    createTask,   deleteTask,   getAllTasks,   updateTask,
      createTask,
      deleteTask,
      getAllTasks,
      updateTask,

--- controllers/task.js
+++ controllers/task.js
@@ -16,7 +16,7 @@ export const getAllTasks = async (req, res, next) => {
   //   const userId = req.user._id;
   //   const tasks = await Task.find({ user: userId });
 
-//   const { id } = req.params;
+  //   const { id } = req.params;
 
   const tasks = await Task.find({ user: req.user._id });
 
@@ -25,4 +25,61 @@ export const getAllTasks = async (req, res, next) => {
     message: "Tasks retrieved successfully",
     tasks: tasks,
   });
+};
+
+export const updateTask = async (req, res) => {
+  const { id } = req.params;
+
+  const { title, description } = req.body;
+
+  const task = await Task.findOneAndUpdate({ id, title, description });
+
+  task.isCompleted = !task.isCompleted;
+
+  await task.save();
+  res.status(200).json({
+    success: true,
+    message: "Task updated successfully",
+  });
+};
+
+// export const updateTask = async (req, res) => {
+//   const { id } = req.params;
+
+//   // Include the update object to specify changes to the document
+//   const task = await Task.findByIdAndUpdate(
+//     id,
+//     { $set: { isCompleted: true } },
+//     { new: true }
+//   );
+
+//   // Check if the task exists
+//   if (!task) {
+//     return res.status(404).json({
+//       success: false,
+//       message: "Task not found",
+//     });
+//   }
+
+//   res.status(200).json({
+//     success: true,
+//     message: "Task updated successfully",
+//     data: task, // Optionally, you can send the updated task data in the response
+//   });
+// };
+
+export const deleteTask = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) return next(new ErrorHandler("Task not found", 404));
+    await task.deleteOne();
+
+    res.status(200).json({
+      message: "Task Deleted!",
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
--- routes/task.js
+++ routes/task.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { createTask, getAllTasks } from "../controllers/task.js";
+import {
+  createTask,
+  deleteTask,
+  getAllTasks,
+  updateTask,
+} from "../controllers/task.js";
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/createTask", isAuthenticated, createTask);
 router.get("/getAllTasks", isAuthenticated, getAllTasks);
+// router.get("/updateTask/:id", isAuthenticated, updateTask);
+
+router.route("/:id").put(updateTask).delete(deleteTask);
 
 export default router;
